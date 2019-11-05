fafcfcf6d5be5d0a3385d7ef2e989c831609f3c3
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-11-05 11:59:01 +0900

    Apply logging library

--- src/main/java/kr/jellyms/custommapper/controller/TodoController.java
+++ src/main/java/kr/jellyms/custommapper/controller/TodoController.java
@@ -2,6 +2,7 @@ package kr.jellyms.custommapper.controller;
 
 import kr.jellyms.custommapper.dto.TodoDto;
 import kr.jellyms.custommapper.model.Todo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos")
+@Slf4j
 public class TodoController {
 
     @PostMapping("")
     public ResponseEntity<Todo> create(@RequestBody TodoDto.Create todo) {
-        System.out.println("Title: " + todo.getTitle());
-        System.out.println("Description: " + todo.getDescription());
 
-        System.out.println(todo.getTitle().equals(todo.getTitle().trim()));
-        System.out.println(todo.getDescription().equals(todo.getDescription().trim()));
+        log.info("Title: " + todo.getTitle());
+        log.info("Description: " + todo.getDescription());
+
+        log.info(String.valueOf(
+                todo.getTitle().equals(todo.getTitle().trim())));
+        log.info(String.valueOf(
+                todo.getDescription().equals(todo.getDescription().trim())));
 
 
         return new ResponseEntity<>(HttpStatus.CREATED);
